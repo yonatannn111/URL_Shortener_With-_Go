6ccd6f66eab4b1ca14803bf57d6914c9a6ab2dd7
yonatannn111 <yonitiman@gmail.com>
2025-09-25 22:12:37 +0300

    creating app.jsx file

---
+++ url-shortener-frontend/src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+function App() {
+  const [url, setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:8080/shorten", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url }),
+      });
+
+      const data = await res.json();
+      setShortUrl(data.shortUrl);
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Go URL Shortener</h1>
+
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          placeholder="Enter your long URL..."
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          required
+        />
+        <button type="submit">Shorten</button>
+      </form>
+
+      {shortUrl && (
+        <div className="result">
+          <p>✅ Shortened URL:</p>
+          <a href={shortUrl} target="_blank" rel="noopener noreferrer">
+            {shortUrl}
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
+
